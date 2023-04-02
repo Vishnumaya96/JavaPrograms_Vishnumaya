f6bcc06b59c4da3745a586a9c29c8052b9ddedda
Vishnumaya96 <mvishnumaya@gmail.com>
2023-04-02 19:00:56 +0530

    Java Programs done on 2 April 2023

--- src/Programs/DuplicateCharsinaString.java
+++ src/Programs/DuplicateCharsinaString.java
@@ -9,13 +9,14 @@ public class DuplicateCharsinaString {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the string");
 		String str1=sc.nextLine();
+		String str2=str1.toUpperCase(); 
 		
 		System.out.println("Duplicate characters are ");
 		int count=0;
 		
 		for(int i=0;i<str1.length();i++) {
 			for(int j=i+1;j<str1.length();j++) {
-				if(str1.charAt(i)==str1.charAt(j)) {
+				if(str2.charAt(i)==str2.charAt(j)) {
 					System.out.println(str1.charAt(i));
 					count++;
 					break;
